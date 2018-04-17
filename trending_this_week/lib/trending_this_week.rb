e30eb48e743be19c6a9039cd0b4a5d4442b55ece
Vanessa Toro <vanetoro84@gmail.com>
2018-04-17 16:22:30 +0000

    call and list methods are setup

--- trending_this_week/lib/trending_this_week.rb
+++ trending_this_week/lib/trending_this_week.rb
@@ -1,6 +1,4 @@
 require_relative "./trending_this_week/version"
 require_relative "./trending_this_week/cli"
-
-module TrendingThisWeek
-  # Your code goes here...
-end
+require_relative "./trending_this_week/spots"
+require_relative "./trending_this_week/scraper"
--- trending_this_week/lib/trending_this_week/cli.rb
+++ trending_this_week/lib/trending_this_week/cli.rb
@@ -1,5 +1,40 @@
 class TrendingThisWeek::CLI
   def call
-    puts 'Welcome to Trending Spots'
-  end  
+    list_spots
+    menu
+
+end
+
+
+def list_spots
+  puts 'These are the spots trending in NYC this week
+
+        1.Mikkeller Brewing NYC
+
+        2.Urbanspace at 570 Lex Food Court
+
+        3.Davelle Café'
+
+    end
+
+  def menu
+    puts 'Enter a number matching a Trending Spot for more information, type list to see the list again or type exit'
+    answer = nil
+      while answer != 'exit'
+        answer = gets.strip.downcase
+        case answer
+            when '1'
+              puts ' Located in Flushing Meadows-Corona Park'
+            when '2'
+              puts ' Located in Midtown East'
+            when '3'
+              puts ' Located in Lower East Side'
+            when 'list'
+              list_spots
+            else
+              puts "Not sure what you want, please try again :)"
+        end
+      end
+      puts 'See you later!'
+  end
 end
