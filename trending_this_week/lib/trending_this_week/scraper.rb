1ca6bfa5fd974f23f2c842a214310a8b1623552b
Vanessa Toro <vanetoro84@gmail.com>
2018-04-17 19:45:11 +0000

    Done.

--- trending_this_week/lib/trending_this_week/scraper.rb
+++ trending_this_week/lib/trending_this_week/scraper.rb
@@ -11,6 +11,17 @@ class TrendingThisWeek::Scraper
     html = open('https://foursquare.com/foursquare/list/trending-this-week-new-york-city')
 
     trending = Nokogiri::HTML(html)
-    binding.pry
+
+      binding.pry
+      loop_this = trending.css('.hotThisWeekList tbody tr')
+      # .each do | spot|
+      binding.pry
+      # rank = index + 1
+      url =  spot.attribute('href').value
+      name = spot.text
+      neighborhood =
+      binding.pry
+    # end
+    # binding.pry
   end
 end
