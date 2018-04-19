0f0cc842d2f263c12cff8f0f50a61ba3b91045f7
Vanessa Toro <vanetoro84@gmail.com>
2018-04-19 16:30:11 +0000

    Working: additional info, going back to city and exit

--- trending_this_week/lib/trending_this_week/cli.rb
+++ trending_this_week/lib/trending_this_week/cli.rb
@@ -2,22 +2,18 @@ class TrendingThisWeek::CLI
     attr_accessor :name, :location, :type, :rank, :rank_change, :url, :address, :city, :phone_number, :features
 
   def call
-    more_info
+  puts  'Welcome to trending spots where you can see the top trending spots from the tops cities in the US'
+    cities
   end
 
   def cities
-    puts 'Welcome to trending spots where you can see the top trending spots from the tops cities in the US
-    Please pick a city
+    puts " Please pick a city
     1. NYC
     2. Los Angeles
     3. Chicago
     4. San Francisco
-    5. Austin'
+    5. Austin"
     picked_city = STDIN.gets.strip
-
-end
-
-  def list_spots(picked_city)
     city_name = nil
     case picked_city
       when '1'
@@ -39,64 +35,61 @@ end
         goodbye
       else
         puts "I didn't get that, please try again!"
-
-    end
-    # binding.pry
-    puts "Here are the top spots in #{city_name.upcase}"
-    TrendingThisWeek::Spots.this_week(url).each do |spot|
-              puts "#{spot.rank}. #{spot.name} - #{spot.type} - #{spot.location}"
+        cities
       end
-
+    list_spots(url,city_name)
 end
 
-  def more_info
-    instance_array = list_spots(cities)
-    puts 'Enter a number matching a Trending Spot for more information, type list to see the list again or type exit'
+  def list_spots(url, city_name)
+        puts "Here are the top spots in #{city_name}"
+  spots_array =   TrendingThisWeek::Spots.this_week(url).each do |spot|
+              puts "#{spot.rank}. #{spot.name} - #{spot.type} - #{spot.location}"
+      end
+  more_info(spots_array)
+end
 
-    user_input = STDIN.gets.to_i - 1
-    user_pick = instance_array[user_input]
-    TrendingThisWeek::Spots.spot_more_info(user_pick)
-    # binding.pry
-    puts "What additional information would you like to know?
-    1. Phone number
-    2. Address
-    3. City
-    4. Features
-    or all to list all the info.
-    "
-    user_choice = gets
-    list_info(user_pick, user_choice)
-    # binding.pry
+  def more_info(spots_array)
+    puts 'Enter a number matching a Trending Spot for more information, type list to see the available cities again or type exit'
+    user_input = STDIN.gets
+    if user_input == 'list'
+      cities
+    elsif user_input == 'exit'
+      goodbye
+    else
+      user_input = user_input.to_i
+      user_pick = spots_array[user_input]
+      TrendingThisWeek::Spots.spot_more_info(user_pick)
+      additional_info(user_pick)
+    end
   end
 
-  def list_info(spot_instance, more_info)
-    more_info = more_info.strip
-      case more_info
+  def list_info(spot_instance, spot_info)
+    spot_info = spot_info.strip
+      case spot_info
         when '1'
           puts "#{spot_instance.phone_number}"
-          more_info = gets
+          additional_info(spot_instance)
         when '2'
           puts "#{spot_instance.address}"
-          more_info = gets
+          additional_info(spot_instance)
         when '3'
           puts "#{spot_instance.city}"
-          more_info = gets
+          additional_info(spot_instance)
         when '4'
             feature(spot_instance)
-            more_info = gets
+            additional_info(spot_instance)
         when 'all'
           puts "Rank #{spot_instance.rank} - #{spot_instance.name} - #{spot_instance.type}
           Address: #{spot_instance.address} - #{spot_instance.location} - #{spot_instance.city}
           Phone: #{spot_instance.phone_number}".strip
           feature(spot_instance)
-          more_info = gets
         when 'exit'
-            other_info = 'exit'
             goodbye
+        when 'city'
+        cities
         else
-          more_info = gets
-
-      end
+          spot_info = gets.strip
+        end
   end
 
   def feature(instance)
@@ -111,5 +104,17 @@ end
 
   def goodbye
     puts 'See you next time!'
+  end
+
+  def additional_info(instance)
+    puts "What additional information would you like to know about #{instance.name}?
+    1. Phone number
+    2. Address
+    3. City
+    4. Features
+    Type 'all' to list all the info or 'city' to see a list of the available cites.
+    Type 'exit' at any time to leave"
+    user_choice = gets.strip
+    list_info(instance,user_choice)
   end
 end
